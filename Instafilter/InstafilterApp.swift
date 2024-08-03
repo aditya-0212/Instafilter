0c61ff55c3811c23287f86ad4e0eab4f289daea6
aditya choubisa <adityachoubisa02@gmail.com>
2024-08-03 11:10:19 +0530

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by APPLE on 03/08/24.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
